007aec3513a5873ef11a6ab866d338018aee84b8
Thomas Waters <twaters117@gmail.com>
2019-04-07 20:05:31 -0400

    Multiple Fractal Types

--- lsystems.js
+++ lsystems.js
@@ -15,23 +15,54 @@ let h_width;
 let h_height;
 let view_size;
 let h_view_size;
-let view_percent = 0.8;
-let rng;
+let view_percent = 0.7;
 
-var params = {
-  seed : 1,
-  iterates : 1,
-  axiom : 'F++F++F',
-  productions : {
-    'F': 'F-F++F-F',
+const lsystems = {
+  'Kosh Snowflake' : {
+    init_angle : 0,
+    angle : 60,
+    axiom : 'F++F++F',
+    productions : {
+      'F': 'F-F++F-F'
+    }
+  },
+  'Sierpinski Triangle' : {
+    init_angle : 0,
+    angle : 120,
+    axiom : 'F-G-G',
+    productions : {
+      'F' : 'F-G+F+G-F',
+      'G' : 'GG'
+    }
+  },
+  'Dragon Curve' : {
+    init_angle : 0,
+    angle : 90,
+    axiom : 'FX',
+    productions : {
+      'X' : 'X+YF+',
+      'Y' : '-FX-Y'
+    }
   },
 };
 
+const default_alg = 'Kosh Snowflake';
+
+const params = {
+  seed        : 1,
+  iterates    : 1,
+  algorithm   : default_alg,
+  init_angle  : lsystems[default_alg].init_angle,
+  angle       : lsystems[default_alg].angle,
+  axiom       : lsystems[default_alg].axiom,
+  productions : lsystems[default_alg].productions,
+};
+
 function setup() {
-  setupSeed();
   setUpGui();
   resize();
   noLoop();
+  angleMode(DEGREES);
 }
 
 function resize() {
@@ -50,15 +81,29 @@ function resizeAndRedraw() {
 }
 
 function setUpGui() {
-  const gui = new dat.GUI({name : 'L-Systems'});
+  const gui = new dat.GUI({
+    name : 'L-Systems',
+    load : JSON
+  });
 
-  gui.add(params, "seed", 1, 5, 1).name("RNG Seed").onChange(setupSeed);
-  gui.add(params, "axiom").name("Axiom").onChange(draw);
-  gui.add(params, "iterates", 1, 5, 1).name("Iterates").onChange(draw);
+  gui.add(params, 'iterates', 1, 10, 1).name('Iterates').onChange(draw);
+  gui.add(params, 'angle', 0, 180, 1).name('Angle').onChange(draw).listen();
+  gui.add(params, 'axiom').name('Axiom').onChange(draw).listen();
+  gui.add(params, 'algorithm', Object.keys(lsystems)).name('Algorithm').onChange(updateAndDraw);
+
+  gui.remember(params);
 }
 
-function setupSeed() {
-  rng = Alea(params.seed);
+function updateAndDraw() {
+  updateAlgorithmParam('init_angle');
+  updateAlgorithmParam('angle');
+  updateAlgorithmParam('axiom');
+  updateAlgorithmParam('productions');
+  draw();
+}
+
+function updateAlgorithmParam(value) {
+  params[value] = lsystems[params.algorithm][value];
 }
 
 function draw() {
@@ -84,23 +129,26 @@ function draw() {
 
 function lsystem() {
   let current_pos = createVector(0, 0);
-  let rotation = 0;
+  let rotation = params.init_angle;
   let lines = [];
+
+  const drawForward = () => {
+    const len = 1 / lsys.iterations + 1;
+    let next_pos = createVector(len, 0);
+    next_pos.rotate(rotation);
+    next_pos.add(current_pos);
+    lines.push([current_pos, next_pos]);
+    current_pos = next_pos;
+  };
 
   let lsys = new LSystem({
     axiom       : params.axiom,
     productions : params.productions,
     finals      : {
-      '+' : () => { rotation +=  Math.PI * 1/3; },
-      '-' : () => { rotation += -Math.PI * 1/3; },
-      'F' : () => {
-        const len = 1 / lsys.iterations + 1;
-        let next_pos = createVector(0, len);
-        next_pos.rotate(rotation);
-        next_pos.add(current_pos);
-        lines.push([current_pos, next_pos]);
-        current_pos = next_pos;
-      }
+      '+' : () => { rotation +=  params.angle; },
+      '-' : () => { rotation += -params.angle; },
+      'F' : () => { drawForward() },
+      'G' : () => { drawForward() },
     },
   });
 
@@ -112,7 +160,6 @@ function lsystem() {
 
 function scaleLines(lines, ibbox) {
   const lbbox = getBbox(lines);
-  console.log(lbbox);
 
   return lines.map(seg => {
     return [
@@ -131,10 +178,10 @@ function scaleLines(lines, ibbox) {
 function getBbox(lines) {
   return lines.reduce((bbox, seg) => {
     return {
-      xmin : Math.min(bbox.xmin, seg[0].x),
-      xmax : Math.max(bbox.xmax, seg[1].x),
-      ymin : Math.min(bbox.ymin, seg[0].y),
-      ymax : Math.max(bbox.ymax, seg[1].y),
+      xmin : Math.min(bbox.xmin, seg[0].x, seg[1].x),
+      xmax : Math.max(bbox.xmax, seg[0].x, seg[1].x),
+      ymin : Math.min(bbox.ymin, seg[0].y, seg[1].y),
+      ymax : Math.max(bbox.ymax, seg[0].y, seg[1].y),
     };
   }, {
     xmin :  Infinity,
